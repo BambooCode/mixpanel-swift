033251604ce17f0832ed17a78199c672d2065e06
Jaime Frutos <jaime@monkeytaps.app>
2024-07-01 21:24:26 +0200

    refactor(): Use SQLCipher if import from SPM

--- Package.swift
+++ Package.swift
@@ -1,6 +1,51 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 
+
+import Foundation
 import PackageDescription
+
+// Don't rely on those environment variables. They are ONLY testing conveniences:
+// $ SQLITE_ENABLE_FTS5=1 SQLITE_ENABLE_PREUPDATE_HOOK=1 make test_SPM
+var swiftSettings: [SwiftSetting] = []
+var cSettings: [CSetting] = []
+if ProcessInfo.processInfo.environment["SQLITE_ENABLE_FTS5"] == "1" {
+    swiftSettings.append(.define("SQLITE_ENABLE_FTS5"))
+}
+if ProcessInfo.processInfo.environment["SQLITE_ENABLE_PREUPDATE_HOOK"] == "1" {
+    swiftSettings.append(.define("SQLITE_ENABLE_PREUPDATE_HOOK"))
+}
+
+swiftSettings.append(contentsOf: [
+    .define("SQLITE_HAS_CODEC"),
+    .define("SQLITE_ENABLE_FTS5")
+])
+
+cSettings.append(contentsOf: [
+    .define("SQLITE_HAS_CODEC"),
+    .define("SQLITE_TEMP_STORE", to: "2"),
+    .define("SQLITE_SOUNDEX"),
+    .define("SQLITE_THREADSAFE"),
+    .define("SQLITE_ENABLE_RTREE"),
+    .define("SQLITE_ENABLE_STAT3"),
+    .define("SQLITE_ENABLE_STAT4"),
+    .define("SQLITE_ENABLE_COLUMN_METADATA"),
+    .define("SQLITE_ENABLE_MEMORY_MANAGEMENT"),
+    .define("SQLITE_ENABLE_LOAD_EXTENSION"),
+    .define("SQLITE_ENABLE_FTS4"),
+    .define("SQLITE_ENABLE_FTS4_UNICODE61"),
+    .define("SQLITE_ENABLE_FTS3_PARENTHESIS"),
+    .define("SQLITE_ENABLE_UNLOCK_NOTIFY"),
+    .define("SQLITE_ENABLE_JSON1"),
+    .define("SQLITE_ENABLE_FTS5"),
+    .define("SQLCIPHER_CRYPTO_CC"),
+    .define("HAVE_USLEEP", to: "1"),
+    .define("SQLITE_MAX_VARIABLE_NUMBER", to: "99999")
+])
+
+let sqlCipherCSettings = cSettings + [
+    .define("NDEBUG"),
+    .define("HAVE_GETHOSTUUID", to: "0")
+]
 
 let package = Package(
     name: "Mixpanel",
@@ -13,21 +58,20 @@ let package = Package(
     products: [
         .library(name: "Mixpanel", targets: ["Mixpanel"])
     ],
-     dependencies: [
-        .package(url: "https://github.com/duckduckgo/GRDB.swift.git", branch: "SQLCipher")
-    ],
     targets: [
         .target(
             name: "Mixpanel",
             path: "Sources",
-            dependencies: [
-                .product(
-                    name: "GRDB", 
-                    package: "GRDB")
-            ],
+            dependencies: ["SQLCipher"],
             resources: [
                 .copy("Mixpanel/PrivacyInfo.xcprivacy")
             ]
-        )
-    ]
+            cSettings: cSettings,
+            swiftSettings: swiftSettings
+        ),
+        .target(
+            name: "SQLCipher",
+            cSettings: sqlCipherCSettings)
+    ],
+    swiftLanguageVersions: [.v5]
 )
