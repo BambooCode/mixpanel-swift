aaa4353ed61155d73df74a26dbf3f956f25ff7c8
Jaime Frutos <jaime@monkeytaps.app>
2024-07-01 13:58:01 +0200

    refactor(): Use SQLCipher if import from SPM

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .library(name: "Mixpanel", targets: ["Mixpanel"])
     ],
      dependencies: [
-        .package(url: "https://github.com/duckduckgo/GRDB.swift", from: "2.3.0"),
+        .package(url: "https://github.com/duckduckgo/GRDB.swift", .branch("SQLCipher")),
     ],
     targets: [
         .target(
