bdef8db54d2ec63a2cf1e43887368152089ee1f7
Jaime Frutos <jaime@monkeytaps.app>
2024-07-18 10:35:10 +0200

    dependency(): update branch to use updated sqlcipher

--- Package.swift
+++ Package.swift
@@ -59,7 +59,7 @@ let package = Package(
         .library(name: "Mixpanel", targets: ["Mixpanel"])
     ],
     dependencies: [
-        .package(url: "https://github.com/BambooCode/GRDB.swift.git", branch: "SQLCipher")
+        .package(url: "https://github.com/BambooCode/GRDB.swift.git", branch: "SQLCipher_updated")
     ],
     targets: [
         .target(
