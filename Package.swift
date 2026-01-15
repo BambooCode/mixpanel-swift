6bf0b85ec90844db3e162935e69e747aeaace4f8
Jaime Frutos <jaime@monkeytaps.app>
2026-01-15 20:56:58 +0100

    feat(): update package.swift to use new grdb and sqlcipher versions

--- Package.swift
+++ Package.swift
@@ -59,7 +59,7 @@ let package = Package(
         .library(name: "Mixpanel", targets: ["Mixpanel"])
     ],
     dependencies: [
-        .package(url: "https://github.com/BambooCode/GRDB.swift.git", branch: "SQLCipher_with_grdb_updated")
+        .package(url: "https://github.com/BambooCode/GRDB.swift.git", revision: "cf51ce2ae5aecd551fadbb50734793bf99497c92")
     ],
     targets: [
         .target(
