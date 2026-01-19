21305e3636554d1af14893af1c23b3c8b8f9efa6
Jaime Frutos <jaime@monkeytaps.app>
2026-01-19 23:16:13 +0100

    fix(): package swift typo

--- Package.swift
+++ Package.swift
@@ -59,7 +59,7 @@ let package = Package(
         .library(name: "Mixpanel", targets: ["Mixpanel"])
     ],
     dependencies: [
-        .package(url: "https://github.com/BambooCode/GRDB.swift.git", exact: : "0.1.0-sqlcipher")
+        .package(url: "https://github.com/BambooCode/GRDB.swift.git", exact: "0.1.0-sqlcipher")
     ],
     targets: [
         .target(
