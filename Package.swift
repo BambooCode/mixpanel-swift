3a2806a2d2dcc1252cae065dbd85b60c78f9b815
Jaime Frutos <jaime@monkeytaps.app>
2026-01-19 23:13:52 +0100

    chore(): update package to point to exact tag version

--- Package.swift
+++ Package.swift
@@ -59,7 +59,7 @@ let package = Package(
         .library(name: "Mixpanel", targets: ["Mixpanel"])
     ],
     dependencies: [
-        .package(url: "https://github.com/BambooCode/GRDB.swift.git", branch: "SQLCipher_with_grdb_updated")
+        .package(url: "https://github.com/BambooCode/GRDB.swift.git", exact: : "0.1.0-sqlcipher")
     ],
     targets: [
         .target(
